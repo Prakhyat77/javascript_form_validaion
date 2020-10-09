e841648cb19201c964f7cea77a21aff9cab1e139
Prakhyat77 <57028200+Prakhyat77@users.noreply.github.com>
2020-10-09 12:49:39 +0530

    Add new js form validation files

---
+++ app.js
@@ -0,0 +1,156 @@
+// const form = document.getElementById("form");
+// const username = document.getElementById("username");
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const password2 = document.getElementById("password2");
+// const phone = document.getElementById("phone");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   checkInputs();
+// });
+
+// function checkInputs() {
+//   // trim to remove the whitespaces
+//   const usernameValue = username.value.trim();
+//   const emailValue = email.value.trim();
+//   const passwordValue = password.value.trim();
+//   const password2Value = password2.value.trim();
+//   const phone = phone.value.trim();
+
+//   if (usernameValue === "") {
+//     setErrorFor(username, "Username cannot be blank");
+//   } else {
+//     setSuccessFor(username);
+//   }
+
+//   if (emailValue === "") {
+//     setErrorFor(email, "Email cannot be blank");
+//   } else if (!isEmail(emailValue)) {
+//     setErrorFor(email, "Not a valid email");
+//   } else {
+//     setSuccessFor(email);
+//   }
+
+//   if (passwordValue === "") {
+//     setErrorFor(password, "Password cannot be blank");
+//   } else {
+//     setSuccessFor(password);
+//   }
+
+//   if (password2Value === "") {
+//     setErrorFor(password2, "Password2 cannot be blank");
+//   } else if (passwordValue !== password2Value) {
+//     setErrorFor(password2, "Passwords does not match");
+//   } else {
+//     setSuccessFor(password2);
+//   }
+
+//   if (phone === "") {
+//     setErrorFor(phone, "Phone Number cannot be blank");
+//   } else if (phone.length < 10) {
+//     setErrorFor(phone, "Phone Number cannot be less than 10 digit");
+//   } else if (phone.length > 10) {
+//     setErrorFor(phone, "Phone Number cannot be more than 10 digit");
+//   } else {
+//     setSuccessFor(phone);
+//   }
+// }
+
+// function setErrorFor(input, message) {
+//   const formControl = input.parentElement;
+//   const small = formControl.querySelector("small");
+//   formControl.className = "form-control error";
+//   small.innerText = message;
+// }
+
+// function setSuccessFor(input) {
+//   const formControl = input.parentElement;
+//   formControl.className = "form-control success";
+// }
+
+// function isEmail(email) {
+//   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+//     email
+//   );
+// }
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+const phn = document.getElementById("phone");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  // trim to remove the whitespaces
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+  const phnValue = phn.value.trim();
+  phnlength = phnValue.toString().length;
+
+  if (usernameValue === "") {
+    setErrorFor(username, "Username cannot be blank");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be blank");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Not a valid email");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be blank");
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (phnValue === "") {
+    setErrorFor(phn, "Phone Number cannot be blank");
+  } else if (phnlength < 10) {
+    setErrorFor(phn, "Phone Number cannot be less than 10 digit");
+  } else if (phnlength > 10) {
+    setErrorFor(phn, "Phone Number cannot be more than 10 digit");
+  } else {
+    setSuccessFor(phn);
+  }
+
+  if (password2Value === "") {
+    setErrorFor(password2, "Password2 cannot be blank");
+  } else if (passwordValue !== password2Value) {
+    setErrorFor(password2, "Passwords does not match");
+  } else {
+    setSuccessFor(password2);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  formControl.className = "form-control error";
+  small.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
